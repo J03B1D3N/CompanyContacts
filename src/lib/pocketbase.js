59e2ffea19d906bc5969d72f547f66830a3a80c8
Justas Lapiene <justas63@gmail.com>
2023-10-12 15:25:22 +0300

    added correct pb url

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,6 +1,6 @@
 import PocketBase from "../../node_modules/pocketbase/dist/pocketbase.es.mjs";
 
-const pb = new PocketBase("http://127.0.0.1:8090");
+const pb = new PocketBase("https://companycontacts.fly.dev/");
 
 function checkPermissions(demand) {
   if (pb.authStore.isValid) {
